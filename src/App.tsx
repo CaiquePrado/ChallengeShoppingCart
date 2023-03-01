daf50c697b6683fbaa003d55b85f261cf15a7d4f
CaiquePrado <caiqueprado76@gmail.com>
2023-03-01 11:45:32 -0300

    chore: setting apiPoint with axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,18 @@ import { Header } from "./components/Header";
 import { Summary } from "./components/summary";
 import { Table } from "./components/Table";
 import { Title } from "./components/Title";
+
+/*
+? DESAFIO - Shopping Cart:
+Você deve desenvolver um carrinho de compras funcional.
+Funcionalidades que esperamos que você desenvolva:
+todo - inserção de novos produtos no carrinho
+todo - remoção de produtos já inseridos
+todo - alteração de quantidade de cada item
+todo - cálculo do preço total dos itens inseridos
+todo - FUNCIONALIDADE EXTRA: aplicação de cupom de desconto
+*/
+
 export function App() {
   return (
     <>
